5916cc38ba15d7d19a3a588311f87583f54f791d
Minger Lin <mingerlin1001@berkeley.edu>
2021-12-21 22:56:53 -0800

    finish lecture 39

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,14 @@
-export default function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+export default function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
