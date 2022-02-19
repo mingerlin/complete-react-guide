337d96c0397b4b53cd09d3b3db84113cc48c9b91
Minger Lin <mingerlin1001@berkeley.edu>
2022-02-18 21:22:03 -0800

    finish lecture 142

--- section11/src/App.js
+++ section11/src/App.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState();
@@ -12,13 +13,15 @@ function App() {
     setCartIsShown(false);
   };
   return (
-    <Fragment>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </Fragment>
+    <CartProvider>
+      <Fragment>
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler} />
+        <main>
+          <Meals />
+        </main>
+      </Fragment>
+    </CartProvider>
   );
 }
 
